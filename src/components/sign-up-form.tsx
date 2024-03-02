96055e97cb8f303f1521a9e6553a844fab9b3b9d
collins ondeyo <ondeyocollins5@gmail.com>
2024-03-02 08:50:19 +0300

    intergrate react-hook-form in the registeration form

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -1,19 +1,31 @@
+"use client"
+
+import { useForm, SubmitHandler } from "react-hook-form";
+
 export const SignUpForm = () => {
+    const { register, handleSubmit } = useForm<{ name: string; email: string; password: string; }>();
+    const onSubmit: SubmitHandler<{ name: string; email: string; password: string; }> = (data) => {
+        console.log(data)
+    }
     return (
         <form
+            onSubmit={handleSubmit(onSubmit)}
             className="bg-[---color-primary] shadow-lg border px-3 py-5 rounded overflow-hidden flex flex-col justify-center gap-3">
 
             <input
+                {...register("name")}
                 placeholder="name"
                 type="text"
                 className="px-2 py-1 rounded-md border-[black] border-[2px] outline-none"
             />
             <input
+                {...register("email")}
                 placeholder="email"
                 type="email"
                 className="px-2 py-1 rounded-md border-[black] border-[2px] outline-none"
             />
             <input
+                {...register("password")}
                 placeholder="password"
                 type="password"
                 className="px-2 py-1 rounded-md border-[black] border-[2px] outline-none"

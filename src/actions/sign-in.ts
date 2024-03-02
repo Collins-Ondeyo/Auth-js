00c7e065640c229acf1acb85faf6f4717187595c
collins ondeyo <ondeyocollins5@gmail.com>
2024-03-02 16:33:51 +0300

    Finish sign in page, initialize sign in server action

---
+++ src/actions/sign-in.ts
@@ -0,0 +1,12 @@
+"use server"
+
+import { SignInSchemaTypes } from "@/schemas";
+import { CreateFormMessage } from "@/utils";
+
+export const SignInUser = async (
+    data: SignInSchemaTypes,
+    callbackUrl?: string
+) => {
+    console.log(data)
+    return CreateFormMessage({success:true, message:"details submitted"})
+}
--- src/components/sign-in-form.tsx
+++ src/components/sign-in-form.tsx
@@ -4,41 +4,62 @@ import {useForm, SubmitHandler} from "react-hook-form"
 
 import Link from "next/link"
 import { Input } from "./Input-Field"
-import { SignInSchemaTypes } from "@/schemas"
+import { SignInSchema, SignInSchemaTypes } from "@/schemas"
 import { useState, useTransition } from "react"
 import { CreateFormMessage } from "@/utils"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { SignInUser } from "@/actions/sign-in"
+import { FormMessage } from "./form-message"
 
 
 export const SignInForm = () => {
     const [formMessage, setFormMessage] = useState<ReturnType<typeof CreateFormMessage> | undefined>(undefined)
     const [isSubmitting, startSubmitting] = useTransition();
-    const { register, handleSubmit, formState: { errors } } = useForm<SignInSchemaTypes>();
+    const { register, handleSubmit, formState: { errors } } = useForm<SignInSchemaTypes>({
+        resolver: zodResolver(SignInSchema)
+    });
+    const onSubmit: SubmitHandler<SignInSchemaTypes> = (data) => {
+        startSubmitting(async () => {
+            SignInUser(data)
+                .then(res => {
+                    setFormMessage(res)
+                })
+        })
+    }
+
     return (
         <form
+            onSubmit={handleSubmit(onSubmit)}
             className="bg-[---color-primary] shadow-lg border px-3 py-5 rounded overflow-hidden flex flex-col justify-center gap-3"
         >
+
             <Input
+                error={errors.email?.message}
+                {...register("email")}
                 placeholder="email"
                 type="email"
             />
             <Input
+                error={errors.password?.message}
+                {...register("password")}
                 placeholder="password"
                 type="password"
             />
 
+            {formMessage && <FormMessage messageObject={formMessage}/>}
             <button
-                disabled={false}
+                disabled={isSubmitting}
                 className="bg-black text-white rounded-md py-1 cursor-pointer font-semibold disabled:opacity-85 disabled:cursor-progress"
                 type="submit"
             >
-                Submit
+                {isSubmitting ? "Submitting...":"Submit"}
             </button>
 
             <Link
                 className="text-center text-sm text-[blue] underline cursor-pointer"
-                href={"/signin"}
+                href={"/signup"}
             >
-                already signed up
+                not signed up
             </Link>
         </form>
     )

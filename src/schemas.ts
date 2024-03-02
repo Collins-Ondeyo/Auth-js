c063328a49deac8fcf2bd993745362c6f3420e19
collins ondeyo <ondeyocollins5@gmail.com>
2024-03-02 16:13:55 +0300

    create sign in schema

--- src/schemas.ts
+++ src/schemas.ts
@@ -7,3 +7,10 @@ export const SignUpSchema = z.object({
 })
 
 export type SignUpSchemaTypes = z.infer<typeof SignUpSchema>;
+
+export const SignInSchema = z.object({
+    email: z.string().email({ message: "invalid email address" }),
+    password: z.string().min(5, { message: "atleast 5 characters" })
+})
+
+export type SignInSchemaTypes = z.infer<typeof SignInSchema>;

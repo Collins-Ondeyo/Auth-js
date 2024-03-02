3f47e0d49d450e3fcdd63738fc2df9b6dd718677
collins ondeyo <ondeyocollins5@gmail.com>
2024-03-02 22:14:11 +0300

    create settings page

---
+++ src/app/(protected)/settings/page.tsx
@@ -0,0 +1,8 @@
+export default function SettingsPage() {
+    return (
+        <div className="h-[100vh] flex flex-col justify-center gap-2 items-center">
+            <div className="shadow-md border-[2px] border-[black] px-2 py-3 rounded-md">     
+            </div>
+        </div>
+    )
+}

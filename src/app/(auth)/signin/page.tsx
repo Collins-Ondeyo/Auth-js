3cfd1bbcdf1dd9601bfca61f88ed47a6c0ce2833
collins ondeyo <ondeyocollins5@gmail.com>
2024-03-02 16:20:19 +0300

    initilize form message state(useState), submitting state(useTransition) in the sign in form

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,7 +1,9 @@
 import { SignInForm } from "@/components/sign-in-form"
 export default function SignInPage() {
     return (
-        <div>
+        <div
+            className="h-[100vh] flex flex-col justify-center gap-3 items-center">
+            <div className="text-3xl font-semibold">Sign In</div>
             <SignInForm/>
         </div>
     )
--- src/components/sign-in-form.tsx
+++ src/components/sign-in-form.tsx
@@ -1,5 +1,45 @@
+"use client"
+
+import {useForm, SubmitHandler} from "react-hook-form"
+
+import Link from "next/link"
+import { Input } from "./Input-Field"
+import { SignInSchemaTypes } from "@/schemas"
+import { useState, useTransition } from "react"
+import { CreateFormMessage } from "@/utils"
+
+
 export const SignInForm = () => {
+    const [formMessage, setFormMessage] = useState<ReturnType<typeof CreateFormMessage> | undefined>(undefined)
+    const [isSubmitting, startSubmitting] = useTransition();
+    const { register, handleSubmit, formState: { errors } } = useForm<SignInSchemaTypes>();
     return (
-        <form action="">SignIn</form>
+        <form
+            className="bg-[---color-primary] shadow-lg border px-3 py-5 rounded overflow-hidden flex flex-col justify-center gap-3"
+        >
+            <Input
+                placeholder="email"
+                type="email"
+            />
+            <Input
+                placeholder="password"
+                type="password"
+            />
+
+            <button
+                disabled={false}
+                className="bg-black text-white rounded-md py-1 cursor-pointer font-semibold disabled:opacity-85 disabled:cursor-progress"
+                type="submit"
+            >
+                Submit
+            </button>
+
+            <Link
+                className="text-center text-sm text-[blue] underline cursor-pointer"
+                href={"/signin"}
+            >
+                already signed up
+            </Link>
+        </form>
     )
 }
